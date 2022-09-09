b0487eb7bfbb1e8fdc2c76833149866f873e03c9
Amanda Rodrigues Cruz <79768233+dsamandarc@users.noreply.github.com>
2022-09-08 21:34:26 -0300

    Exercise Python I ex11

---
+++ ex11.py
@@ -0,0 +1,6 @@
+# 11)This program will calculate the area of a square. 
+# Ask the person to enter the numerical measurement of one side of the square. 
+# AND then tell it the value of the area of the square.
+side_of_square = input(f'Enter the side of square in cm:')
+area_of_square = float(side_of_square)*float(side_of_square)
+print(f'The area of the square is:', area_of_square)
