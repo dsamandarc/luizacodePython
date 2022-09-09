f7b15af09a894d144e30bf05cb1126e2a6078268
Amanda Rodrigues Cruz <79768233+dsamandarc@users.noreply.github.com>
2022-09-08 22:02:27 -0300

    Exercise Python I ex15

---
+++ ex15.py
@@ -0,0 +1,8 @@
+# 15)For the Python program in the ex15.py file.
+# In a house, a family decided to share the value of the energy bill among the residents of the house. 
+# In the program they inform the value of the energy bill and how many will divide the bill in the month.
+# The program will calculate how much each should contribute to the energy bill.
+energy_bill_value = float(input("Enter the value of your energy bill:"))
+number_of_residents = int(input("Enter the number of residents of the house:"))
+energy_by_residents = energy_bill_value/number_of_residents
+print("Each resident of the house will pay:", energy_by_residents, 'reais')
