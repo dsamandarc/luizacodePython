8d883706d7feb47d3c87533deef105a7450c9c82
Amanda Rodrigues Cruz <79768233+dsamandarc@users.noreply.github.com>
2022-09-08 21:43:37 -0300

    Exercise Python I ex12

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# 12)This program will calculate the area of a triangle. 
+# Ask the person to inform the numerical measurement of the base of the triangle, then collect the height value.
+# Give the value of the area of the triangle.
+base_of_triangle = input('Enter the base of triangle in cm:')
+height_of_triangle = input('Enter the height of triangle in cm:')
+area_of_triangle = ((float(base_of_triangle))*(float(height_of_triangle)))/2
+print(f'The area of the triangle is:', {area_of_triangle}, 'cm^2')
