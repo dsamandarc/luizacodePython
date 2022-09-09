11f05f6ed721a09e7ebfc88ab5ba5b1bd52c49a6
Amanda Rodrigues Cruz <79768233+dsamandarc@users.noreply.github.com>
2022-09-08 21:47:58 -0300

    Exercise Python I ex13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+# Create the following Python program in the ex13.py file.
+# Collect the age of 3 people and show your average age.
+age_1 = int(input("Enter your age:"))
+age_2 = int(input("Enter your age:"))
+age_3 = int(input("Enter your age:"))
+number_of_people = 3
+average_age = (age_1+age_2+age_3)/3
+print(average_age)
