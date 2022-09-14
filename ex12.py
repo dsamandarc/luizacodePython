fbcc27b95078d5046ab3984bcce2037cfc93aa4e
Amanda Rodrigues Cruz <79768233+dsamandarc@users.noreply.github.com>
2022-09-13 23:20:12 -0300

    Exercise 12 Pyhton I

--- ex12.py
+++ ex12.py
@@ -5,3 +5,6 @@ base_of_triangle = input('Enter the base of triangle in cm:')
 height_of_triangle = input('Enter the height of triangle in cm:')
 area_of_triangle = ((float(base_of_triangle))*(float(height_of_triangle)))/2
 print(f'The area of the triangle is:', {area_of_triangle}, 'cm^2')
+#return: Enter the base of triangle in cm:8
+# Enter the height of triangle in cm:6
+# The area of the triangle is: {24.0} cm^2
